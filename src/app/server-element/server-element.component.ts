1d793f097cee7a3d649043be35514a09d0d526a0
jiman93 <zulhafiz06@gmail.com>
2017-09-11 14:40:20 +0800

    First Commit

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component, OnInit, Input, ViewChild, ElementRef, ContentChild 
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html'
+})
+export class ServerElementComponent {
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+}
